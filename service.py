784ad6881d9b035ad9c5e3e99a5180086d29c63c
jsk1465 <jon.kirkpatrick@sbcglobal.net>
2019-12-18 10:59:42 -0600

    created the flask service with templaces

--- service.py
+++ service.py
@@ -1,1 +1,33 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify,request
+
+app = Flask(__name__)
+
+API_ROUTE = "/api/v1/resources/"
+
+data = [{
+        'fName':'Jon',
+        'lName':"Kirk",
+    },{
+        'fName':'Yemi',
+        'lName':'Orekoya'
+    }]
+
+def getAPIRoute(line):
+    return API_ROUTE + line
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route(getAPIRoute("data/all"),methods=['GET'])
+def api_test():
+    print(request.args)
+    if('fName' in request.args):
+        fName = request.args['fName']
+    else:
+        return jsonify(data)
+    results = [item for item in data if fName==item['fName']]
+    return jsonify(results)
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
